5f61847a702902779060a924c25cc89dd313953d
FLEXPAD-AP\Anna <anna.poglitsch@stud.fh-campuswien.ac.at>
2023-03-07 01:19:42 +0100

    Anna - case-sensitive is no issue anymore

--- src/main/java/com/example/Semicolon/HomeController.java
+++ src/main/java/com/example/Semicolon/HomeController.java
@@ -42,16 +42,16 @@ public class HomeController implements Initializable {
     private String searchWord = "";
 
     @FXML
-    private void activateMenu(){
+    private void activateMenu() {
         TranslateTransition tt = new TranslateTransition();
         tt.setNode(menu);
         tt.setDuration(Duration.millis(500));
-        if(menuActive){
+        if (menuActive) {
             tt.setFromY(menu.getHeight());
             tt.setToY(0);
             menu.setDisable(true);
             menuActive = false;
-        }else {
+        } else {
             tt.setFromY(0);
             tt.setToY(menu.getHeight());
             menu.setDisable(false);
@@ -66,56 +66,59 @@ public class HomeController implements Initializable {
         genresChoice.setValue("---All GENRES---");
         sortingChoice.setItems(sortingKeywords);
         sortingChoice.setValue("---NO SORTING---");
-        if(originalMovieList != null) {
+        if (originalMovieList != null) {
             movieList.addAll(originalMovieList);
             movieDisplay.setItems(movieList);
             movieDisplay.setCellFactory(movieDisplay -> new MovieCard());
         }
         sortingChoice.setOnAction(this::sortMoviesByTitle);
         genresChoice.setOnAction(this::sortMovieGenres);
-   }
+        searchField.setOnKeyPressed(event -> {
+            searchMovie();
+        });
+    }
 
     private ObservableList<Movie> sortMoviesByTitle(Event event) {
         if (sortingChoice.getValue().equals("A-Z")) {
-        Collections.sort(movieList, new Comparator<Movie>() {
-            @Override
-            public int compare(Movie o1, Movie o2) {
-                return o1.title.compareTo(o2.title);
-            }
-        });
-        }
-        else if (sortingChoice.getValue().equals("Z-A")) {
             Collections.sort(movieList, new Comparator<Movie>() {
-            @Override
-            public int compare(Movie o1, Movie o2) {
-                return o2.title.compareTo(o1.title);
-            }
-        });
+                @Override
+                public int compare(Movie o1, Movie o2) {
+                    return o1.title.compareTo(o2.title);
+                }
+            });
+        } else if (sortingChoice.getValue().equals("Z-A")) {
+            Collections.sort(movieList, new Comparator<Movie>() {
+                @Override
+                public int compare(Movie o1, Movie o2) {
+                    return o2.title.compareTo(o1.title);
+                }
+            });
 
-        }
-        else if(sortingChoice.getValue().equals("---NO SORTING---")) {
+        } else if (sortingChoice.getValue().equals("---NO SORTING---")) {
             movieList.clear();
             movieList.addAll(originalMovieList);
             return movieList;
         }
         return null;
     }
-    private ObservableList<Movie> sortMovieGenres(ActionEvent event) {
-       movieList.removeAll();
-       for (Movie movie : originalMovieList) {
 
-       }
-       return movieList;
+    private ObservableList<Movie> sortMovieGenres(ActionEvent event) {
+        movieList.removeAll();
+        for (Movie movie : originalMovieList) {
+
+        }
+        return movieList;
     }
+
     @FXML
-    private ObservableList<Movie> searchMovie(ActionEvent event) {
+    private ObservableList<Movie> searchMovie() {
         movieList.clear();
-           for (int i = 0; i < originalMovieList.size(); i++) {
-               if (originalMovieList.get(i).description.contains(searchField.getText()) ||
-                      originalMovieList.get(i).title.contains(searchField.getText())) {
-                   movieList.add(originalMovieList.get(i));
-               }
-           }
-           return movieList;
+        for (int i = 0; i < originalMovieList.size(); i++) {
+            if (originalMovieList.get(i).description.toLowerCase().contains(searchField.getText()) ||
+                    originalMovieList.get(i).title.toLowerCase().contains(searchField.getText())) {
+                movieList.add(originalMovieList.get(i));
+            }
+        }
+        return movieList;
     }
-    }
+}
